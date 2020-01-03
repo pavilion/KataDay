11cb38399a54f37b3aa30b3d967bc1d2ca96f5bb
Ismael Oliva <ismael.oliva@digiseq.co.uk>
2020-01-03 23:21:30 +0000

    Not http lol

--- UniqueInOrder.js
+++ UniqueInOrder.js
@@ -1,6 +1,6 @@
-//www.codewars.com/trainer/javascript
+//https://www.codewars.com/trainer/javascript
 
-https: function uniqueInOrder(iterable) {
+function uniqueInOrder(iterable) {
   return typeof iterable === "string"
     ? iterable.split("").filter((e, i) => e !== iterable[i - 1])
     : iterable.filter((e, i) => e !== iterable[i - 1]);
